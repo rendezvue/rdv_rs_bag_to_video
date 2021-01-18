3633dd141630f3339c38b486fe08455bb51368af
Juhong <juhong@rdv.ai>
2021-01-18 12:20:05 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include <librealsense2/rsutil.h>
+
+#include <opencv2/opencv.hpp>   
+
+#include <fstream>              // File IO
+#include <iostream>             // Terminal IO
+#include <sstream>              // Stringstreams
+#include <algorithm>
+#include <cstring>
+
+int main(int argc, char * argv[])
+{
+    return EXIT_SUCCESS;
+}
